d649cdd21515ec25eba7c76f4ab44619f2200983
twinitiee <twinitiee@gmail.com>
2018-07-24 17:56:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 x = gets.strip.to_i
 
-if x % 3 == 0 
+if x % 15 == 0 
+print "fizzbuzz"
+elsif x % 3 == 0 
   print "fizz"
 elsif x % 5 == 0
 print "buzz"
-elsif x % 15 == 0 
-print "fizzbuzz"
 else
   print x 
 end
